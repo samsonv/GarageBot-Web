21c9cc12696ba1258d7d04c1d1490c436fd6f225
Samson Valland <samsonvalland@gmail.com>
2016-02-01 22:32:51 +0100

    Socket.io in place!

--- server.js
+++ server.js
@@ -1,50 +1,26 @@
 var app = require('express')();
-var moment = require('moment');
-var got = require('got');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
-var timeSinceLastPhone = moment();
-var garageAdress = '';
-var garagePort = '3000';
-var port = process.env.PORT || 1337;
+var moment = require('moment');
+var got = require('got');
 
 app.get('/', function(req, res){
-    res.send('Hello world!');
-})
-
-app.get('/status', function(req, res){
-    res.send('Time since hello: ' + timeSinceLastPhone.from(moment()));
-})
-
-app.get('/push/:seconds', function(req, res){
-    var seconds = parseInt(req.params.seconds) || 2;
-    var address = 'http://' + garageAdress + ':' + garagePort + '/blink/' + seconds        
-    console.log('should blink', seconds, address);
-    got(address)
-    .then(response => {
-        console.log(response.body);
-        res.send('Sim sala bim')
-    })
-    .catch(error => {
-        console.log(error.response.body);
-    });
+  res.sendFile(__dirname + '/index.html');
 });
-
-app.get('/phonehome', function(req, res){
-    garageAdress = req.connection.remoteAddress.split(':').pop();
-    console.log('The garage opener phoned home from ' + garageAdress);
-    timeSinceLastPhone = moment();
-    res.send("ok");
-})
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+    io.emit('web', 'someone connected');
+    
+    socket.on('pi', function(msg){
+        console.log('Pi tells me: ' + msg);
+        io.emit('web', 'Pi says ' + msg);
+    });
+    
+    socket.on('web message', function(msg){
+        io.emit('command', msg)
+    })
 });
 
-app.listen(port, function () {
-  console.log('Example app listening on port ', port);
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
