fe4d050546c7bf42f72d30c32c46a2659f6c67a7
Samson Valland <samsonvalland@gmail.com>
2016-04-03 14:22:33 +0200

    10 last messages from pi + distance bar!

--- server.js
+++ server.js
@@ -13,38 +13,45 @@ this.messages = [];
 
 app.use(express.static('wwwroot'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/messages', function(req, res){
+app.get('/messages', function(req, res) {
     res.send(self.messages);
 })
 
 
-io.on('connection', function (socket) {
+io.on('connection', function(socket) {
     io.emit('web', 'someone connected');
 
-    socket.on('pi', function (msg) {
+    socket.on('pi', function(msg) {
         saveMessage(msg);
         io.emit('web', 'Pi says ' + msg);
     });
+    
+    socket.on('distance-message', function(msg) {
+        io.emit('distance', msg);
+    })
 
-    socket.on('web message', function (msg) {
+    socket.on('web message', function(msg) {
         io.emit('command', msg)
     })
 
-    socket.on('disconnect', function () { });
+    socket.on('disconnect', function() { });
 });
 
-function saveMessage(msg){
-    if (self.messages.length > 10){
+function saveMessage(msg) {
+    if (self.messages.length > 10) {
         self.messages.shift();
     }
-    self.messages.push(msg)
+    self.messages.push({
+        time: moment().format('DD/MM/YYYY HH:mm'),
+        message: msg
+    });
 }
 
 
-http.listen(port, function () {
+http.listen(port, function() {
     console.log('listening on *:' + port);
 });
--- wwwroot/app.js
+++ wwwroot/app.js
@@ -1,20 +1,34 @@
 var socket = io();
 
-$('form').submit(function () {
+$('form').submit(function() {
     socket.emit('web message', $('#m').val());
     $('#m').val('');
     return false;
 });
 
-$('#open-button').click(function () {
+$('#open-button').click(function() {
     socket.emit('web message', 'blink');
     return false;
 })
 
-socket.on('distance-message', function(msg){
+var addMessage = function(msg) {
+    $('#messages').prepend($('<li class="list-group-item">')
+        .text(msg.time + ': ' + msg.message));
+}
+
+var getMessages = function() {
+    $('#messages').html("");
+    $.get("/messages").done(function(data) {
+        data.map(addMessage);
+    })
+}
+
+socket.on('distance', function(msg) {
+    console.log('got distance', msg);
     $('#distance').text(msg);
+    $('#progress').val(msg);
 })
 
-socket.on('web', function (msg) {
-    $('#messages').prepend($('<li class="list-group-item">').text(msg));
-});
+socket.on('web', getMessages);
+
+getMessages();
